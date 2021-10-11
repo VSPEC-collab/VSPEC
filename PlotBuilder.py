6fbc21907585b35ccec6ed164ad346a77f863856
ckelahan <cameron.m.kelahan@nasa.gov>
2021-10-11 14:23:39 -0400

    outlining how the directory structure of saved Data that will be used while graphing

--- PlotBuilder.py
+++ PlotBuilder.py
@@ -2,6 +2,16 @@ import SpectraBuilder
 import matplotlib.pyplot as plt
 import statistics
 
+# data to save for use in plots:
+# 1) adjusted total planet flux (combination of thermal and variable stellar reflected planet)
+# 2) sumflux of star for each phase facing observer
+# 3) sumflux of star for each phase facing planet
+# 4) 
+
+# Strategy for saving this info
+# Save in seperate folders with all phase's labeled as "descriptiveName_phase.txt" where phase is the current phase
+# in integer form
+
 def planet_plots(allModels):
     # Create a copy of the original PSG planet flux values to compare the variable stellar adjusted planet values to
     # at the end
@@ -10,7 +20,7 @@ def planet_plots(allModels):
 
 # Plot a time-series light curve as the star rotates
 def plot_light_curve(self, count, x, y):
-    # Calculate the mean value for the current bin, for the current star hemisphere
+    # Calculate the mean value for the current phase, for the current star hemisphere
     avg_sumflux = statistics.mean(self.modelspectra.sumflux)
 
     # The X-Axis represents the number of the images taken. Presents itself as a time series, from first image
