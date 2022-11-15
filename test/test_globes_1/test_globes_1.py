862892b8c71fe14a93886c55483b0a99f93bf6d9
Ted Johnson <tedjohnson12@g.ucla.edu>
2022-11-15 12:48:56 -0500

    globes test 1 building

--- test/test_globes_1/test_globes_1.py
+++ test/test_globes_1/test_globes_1.py
@@ -6,9 +6,19 @@
 
 from pathlib import Path
 import matplotlib.pyplot as plt
+from astropy import units as u
 
 
 from VSPEC import ObservationModel, PhaseAnalyzer
 
-CONFIG_PATH = Path(__file__).parent 
+CONFIG_FILENAME = 'test_globes_1.cfg'
+CONFIG_PATH = Path(__file__).parent / CONFIG_FILENAME
+
+model = ObservationModel(CONFIG_PATH)
+model.build_directories()
+model.build_star()
+model.warm_up(0*u.day,0*u.day)
+model.bin_spectra()
+model.build_planet()
+model.build_spectra()
 
