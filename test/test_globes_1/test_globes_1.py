4f839a35ffbeb45ae4b345c8c94df9a08f229dd9
Ted Johnson <tedjohnson12@g.ucla.edu>
2022-11-15 12:34:39 -0500

    restructure test

---
+++ test/test_globes_1/test_globes_1.py
@@ -0,0 +1,14 @@
+# GLOBES TEST 1
+#
+#
+# Test the phase dependence of reflected light
+##############################################
+
+from pathlib import Path
+import matplotlib.pyplot as plt
+
+
+from VSPEC import ObservationModel, PhaseAnalyzer
+
+CONFIG_PATH = Path(__file__).parent 
+
