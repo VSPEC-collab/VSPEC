7286db2c27bf9e20576f801ffee0d428015e35b8
Ted Johnson <tedjohnson12@g.ucla.edu>
2023-03-03 13:16:59 -0500

    module docstring

--- VSPEC/__init__.py
+++ VSPEC/__init__.py
@@ -1,2 +1,13 @@
+"""
+VSPEC: Variable Star Phase Curve
+
+VSPEC is designed as an observation simulation suite for rocky
+exoplanets orbiting variable stars. Built for the Mid-IR Exoplanet
+CLimate Explorer (MIRECLE) mission concept, this package is flexible
+enough to simulate observations over a wide range of conditions.
+
+VSPEC utilizes the Planetary Spectrum Generator (PSG, psg.gsfc.nasa.gov)
+to perform radiative transfer calculations
+"""
 from VSPEC.main import ObservationModel
 from VSPEC.analysis import PhaseAnalyzer
