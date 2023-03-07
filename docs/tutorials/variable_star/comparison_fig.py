0a10b919025964fe9215693373e395a138ba4589
Ted Johnson <tedjohnson12@g.ucla.edu>
2023-03-07 11:30:10 -0500

    update comparison figure

--- docs/tutorials/variable_star/comparison_fig.py
+++ docs/tutorials/variable_star/comparison_fig.py
@@ -16,9 +16,9 @@ if __name__ in '__main__':
 
     fig,axes = plt.subplots(3,1,figsize=(4,7),sharex=True)
     for ax,data,scale in zip(axes,[spot_data,fac_data,flare_data],[100,1e6,100]):
-        ax.plot(data.time.to(u.day),(data.lightcurve('star',0,normalize=0)-1)*scale,
+        ax.plot((data.time - data.time[0]).to(u.day),(data.lightcurve('star',0,normalize=0)-1)*scale,
         label=f'{data.wavelength[0]:.1f}')
-        ax.plot(data.time.to(u.day),(data.lightcurve('star',-6,normalize=0)-1)*scale,
+        ax.plot((data.time-data.time[0]).to(u.day),(data.lightcurve('star',-6,normalize=0)-1)*scale,
         label = f'{data.wavelength[-6]:.1f}')
         ax.set_ylabel(f'contrast ({"pct" if scale==100 else "ppm"})',fontsize=12)
         ax.tick_params(direction='in',right=True,top=True)
