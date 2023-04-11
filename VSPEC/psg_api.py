f481104572d2e7d95676b85de077773892ca3046
Ted Johnson <tedjohnson12@g.ucla.edu>
2023-04-11 12:27:14 -0400

    reformating

--- VSPEC/psg_api.py
+++ VSPEC/psg_api.py
@@ -21,7 +21,7 @@ warnings.simplefilter('ignore', category=u.UnitsWarning)
 
 def call_api(config_path: str, psg_url: str = 'https://psg.gsfc.nasa.gov',
              api_key: str = None, output_type: str = None, app: str = None,
-             outfile: str = None, verbose: bool = False) -> None:
+             outfile: str = None) -> None:
     """
     Call the PSG api
 
@@ -44,7 +44,7 @@ def call_api(config_path: str, psg_url: str = 'https://psg.gsfc.nasa.gov',
         The path to write the PSG output.
     """
     data = {}
-    with open(config_path,'rb') as file:
+    with open(config_path, 'rb') as file:
         dat = file.read()
     data['file'] = dat
     if api_key is not None:
@@ -54,13 +54,13 @@ def call_api(config_path: str, psg_url: str = 'https://psg.gsfc.nasa.gov',
     if output_type is not None:
         data['type'] = output_type
     url = f'{psg_url}/api.php'
-    reply = requests.post(url,data=data,timeout=120)
+    reply = requests.post(url, data=data, timeout=120)
     if outfile is not None:
-        with open(outfile,'w',encoding='UTF-8') as file:
+        with open(outfile, 'w', encoding='UTF-8') as file:
             file.write(reply.text)
-    
 
-def write_static_config(path:Path,params:ParamModel,file_mode:str='w')->None:
+
+def write_static_config(path: Path, params: ParamModel, file_mode: str = 'w') -> None:
     """
     Write the initial PSG configuration file for a phase curve simulation.
     This occurs after the GCM is uploaded and before we start iterating through
@@ -81,40 +81,40 @@ def write_static_config(path:Path,params:ParamModel,file_mode:str='w')->None:
         file.write('<OBJECT>Exoplanet\n')
         file.write(f'<OBJECT-NAME>{params.planet_name}\n')
         file.write('<OBJECT-DIAMETER>%f\n' %
-                    to_float(2*params.planet_radius, u.km))
+                   to_float(2*params.planet_radius, u.km))
         file.write('<OBJECT-GRAVITY>%f\n' % params.planet_grav)
         file.write(f'<OBJECT-GRAVITY-UNIT>{params.planet_grav_mode}\n')
         file.write('<OBJECT-STAR-TYPE>%s\n' % params.psg_star_template)
         file.write('<OBJECT-STAR-DISTANCE>%f\n' %
-                    to_float(params.planet_semimajor_axis, u.AU))
+                   to_float(params.planet_semimajor_axis, u.AU))
         file.write('<OBJECT-PERIOD>%f\n' %
-                    to_float(params.planet_orbital_period, u.day))
+                   to_float(params.planet_orbital_period, u.day))
         file.write('<OBJECT-ECCENTRICITY>%f\n' %
-                    params.planet_eccentricity)
+                   params.planet_eccentricity)
         file.write('<OBJECT-PERIAPSIS>%f\n' %
-                    to_float(params.system_phase_of_periasteron, u.deg))
+                   to_float(params.system_phase_of_periasteron, u.deg))
         file.write('<OBJECT-STAR-TEMPERATURE>%f\n' %
-                    to_float(params.star_teff, u.K))
+                   to_float(params.star_teff, u.K))
         file.write('<OBJECT-STAR-RADIUS>%f\n' %
-                    to_float(params.star_radius, u.R_sun))
+                   to_float(params.star_radius, u.R_sun))
         file.write('<GEOMETRY>Observatory\n')
         file.write('<GEOMETRY-OBS-ALTITUDE>%f\n' %
-                    to_float(params.system_distance, u.pc))
+                   to_float(params.system_distance, u.pc))
         file.write('<GEOMETRY-ALTITUDE-UNIT>pc\n')
         file.write('<GENERATOR-RANGE1>%f\n' %
-                    to_float(params.lambda_min, params.target_wavelength_unit))
+                   to_float(params.lambda_min, params.target_wavelength_unit))
         file.write('<GENERATOR-RANGE2>%f\n' %
-                    to_float(params.lambda_max, params.target_wavelength_unit))
+                   to_float(params.lambda_max, params.target_wavelength_unit))
         file.write(
             f'<GENERATOR-RANGEUNIT>{params.target_wavelength_unit}\n')
         file.write('<GENERATOR-RESOLUTION>%f\n' %
-                    params.resolving_power)
+                   params.resolving_power)
         file.write('<GENERATOR-RESOLUTIONUNIT>RP\n')
         file.write('<GENERATOR-BEAM>%d\n' % params.beamValue)
         file.write('<GENERATOR-BEAM-UNIT>%s\n' % params.beamUnit)
         file.write('<GENERATOR-CONT-STELLAR>Y\n')
         file.write('<OBJECT-INCLINATION>%s\n' %
-                    to_float(params.system_inclination, u.deg))
+                   to_float(params.system_inclination, u.deg))
         file.write('<OBJECT-SOLAR-LATITUDE>0.0\n')
         file.write('<OBJECT-OBS-LATITUDE>0.0\n')
         file.write('<GENERATOR-RADUNITS>%s\n' % params.psg_rad_unit)
@@ -144,6 +144,7 @@ def write_static_config(path:Path,params:ParamModel,file_mode:str='w')->None:
         file.write('<GENERATOR-TELESCOPE2>1.0\n')
         file.write('<GENERATOR-TELESCOPE3>1.0\n')
 
+
 class PSGrad:
     """
     Container for PSG rad files
@@ -156,7 +157,7 @@ class PSGrad:
     data : dict
         Dictionary containing the spectral data. They keys are the column names and the
         values are astropy.units.Quantity arrays.
-    
+
     Attributes
     ----------
     header : dict
@@ -166,72 +167,77 @@ class PSGrad:
         Dictionary containing the spectral data. They keys are the column names and the
         values are astropy.units.Quantity arrays.
     """
-    def __init__(self,header,data):
+
+    def __init__(self, header, data):
         self.header = header
         self.data = data
-    
+
     @classmethod
-    def from_rad(cls,filename):
+    def from_rad(cls, filename):
         """
         Create a `PSGrad` object from a file. This is designed to load in
         the raw `.rad` output from PSG
         """
         raw_header = []
         raw_data = []
-        with open(filename,'r',encoding='UTF-8') as file:
+        with open(filename, 'r', encoding='UTF-8') as file:
             for line in file:
                 if line[0] == '#':
-                    raw_header.append(line.replace('\n',''))
+                    raw_header.append(line.replace('\n', ''))
                 else:
-                    raw_data.append(line.replace('\n',''))
+                    raw_data.append(line.replace('\n', ''))
         header = {
-            'warnings' : [],
-            'errors' : [],
-            'binning' : -1,
-            'author' : '',
-            'date' : '',
-            'velocities' : {},
-            'spectral_unit' : u.dimensionless_unscaled,
-            'radiance_unit' : u.dimensionless_unscaled,
+            'warnings': [],
+            'errors': [],
+            'binning': -1,
+            'author': '',
+            'date': '',
+            'velocities': {},
+            'spectral_unit': u.dimensionless_unscaled,
+            'radiance_unit': u.dimensionless_unscaled,
 
         }
         for i, item in enumerate(raw_header):
             if 'WARNING' in item:
                 warning, kind, message = item.split('|')
-                header['warnings'].append(dict(kind=kind,message=message))
+                header['warnings'].append(dict(kind=kind, message=message))
             elif 'ERROR' in item:
                 error, kind, message = item.split('|')
-                header['errors'].append(dict(kind=kind,message=message))
+                header['errors'].append(dict(kind=kind, message=message))
             elif '3D spectroscopic simulation' in item:
-                header['binning'] = int(re.findall(r'of ([\d]+) \(',item)[0])
+                header['binning'] = int(re.findall(r'of ([\d]+) \(', item)[0])
             elif 'Planetary Spectrum Generator' in item:
                 header['author'] = item[1:].strip()
             elif 'Synthesized' in item:
                 header['date'] = item[1:].strip()
             elif 'Doppler velocities' in item:
-                unit = u.Unit(re.findall(r'\[([\w\d/]+)\]',item)[0])
-                keys = re.findall(r'\(([\w\d, \+]+)\)',item)[0].split(',')
+                unit = u.Unit(re.findall(r'\[([\w\d/]+)\]', item)[0])
+                keys = re.findall(r'\(([\w\d, \+]+)\)', item)[0].split(',')
                 values = item.split(':')[1].split(',')
                 for key, value in zip(keys, values):
                     header['velocities'][key] = float(value)*unit
             elif 'Spectral unit' in item:
-                header['spectral_unit'] = u.Unit(re.findall(r'\[([\w\d/]+)\]',item)[0])
+                header['spectral_unit'] = u.Unit(
+                    re.findall(r'\[([\w\d/]+)\]', item)[0])
             elif 'Radiance unit' in item:
-                header['radiance_unit'] = u.Unit(re.findall(r'\[([\w\d/]+)\]',item)[0])
+                header['radiance_unit'] = u.Unit(
+                    re.findall(r'\[([\w\d/]+)\]', item)[0])
         columns = raw_header[-1][1:].strip().split()
         dat = StringIO('\n'.join(raw_data))
-        df = pd.read_csv(dat,names = columns,delim_whitespace=True)
+        df = pd.read_csv(dat, names=columns, delim_whitespace=True)
         if len(df) == 0:
-            raise ValueError('It looks like there might not be any data in this rad file.')
+            raise ValueError(
+                'It looks like there might not be any data in this rad file.')
         data = {}
         if not columns[0] == 'Wave/freq':
             raise ValueError('.rad format is incorrect')
         data[columns[0]] = df[columns[0]].values * header['spectral_unit']
         for col in columns[1:]:
             data[col] = df[col].values * header['radiance_unit']
-        return cls(header,data)
-                
-def get_reflected(cmb_rad:PSGrad,therm_rad:PSGrad,planet_name:str) -> u.Quantity:
+        return cls(header, data)
+
+
+def get_reflected(cmb_rad: PSGrad, therm_rad: PSGrad, planet_name: str) -> u.Quantity:
     """
     Get reflected spectra.
 
@@ -241,12 +247,12 @@ def get_reflected(cmb_rad:PSGrad,therm_rad:PSGrad,planet_name:str) -> u.Quantity
         A rad file from the star+planet PSG call.
     therm_rad : PSGrad
         A rad file from the planet-only PSG call.
-    
+
     Returns
     -------
     astropy.units.Quantity
         The spectrum of the reflected light.
-    
+
     Raises
     ------
     ValueError
@@ -255,9 +261,9 @@ def get_reflected(cmb_rad:PSGrad,therm_rad:PSGrad,planet_name:str) -> u.Quantity
         If neither object has a `'Reflected'` data array and at least one
         of them is missing the `planet_name` data array.
     """
-    if not np.all(isclose(cmb_rad.data['Wave/freq'],therm_rad.data['Wave/freq'],1e-3*u.um)):
+    if not np.all(isclose(cmb_rad.data['Wave/freq'], therm_rad.data['Wave/freq'], 1e-3*u.um)):
         raise ValueError('The spectral axes must be equivalent.')
-    
+
     if 'Reflected' in cmb_rad.data.keys():
         return cmb_rad.data['Reflected']
     elif 'Reflected' in therm_rad.data.keys():
@@ -266,9 +272,3 @@ def get_reflected(cmb_rad:PSGrad,therm_rad:PSGrad,planet_name:str) -> u.Quantity
         return cmb_rad.data[planet_name] - therm_rad.data[planet_name]
     else:
         raise KeyError(f'Data array {planet_name} not found.')
-        
-                
-
-            
-
-        
