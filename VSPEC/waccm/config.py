998d8449bb2d4e54f1d4bde6363265dfbe58ebbc
Ted Johnson <tedjohnson12@g.ucla.edu>
2023-05-22 17:16:55 -0400

    add OH support

--- VSPEC/waccm/config.py
+++ VSPEC/waccm/config.py
@@ -8,7 +8,8 @@ psg_aerosol_size_unit = u.m
 
 atmosphere_type_dict = {'H2':45,'He':0,'H2O':1,'CO2':2,'O3':3,'N2O':4,'CO':5,'CH4':6,'O2':7,
                         'NO':8,'SO2':9,'NO2':10,'N2':22,'HNO3':12,'HO2NO2':'SEC[26404-66-0] Peroxynitric acid',
-                        'N2O5':'XSEC[10102-03-1] Dinitrogen pentoxide','O':'KZ[08] Oxygen'}
+                        'N2O5':'XSEC[10102-03-1] Dinitrogen pentoxide','O':'KZ[08] Oxygen',
+                        'OH':'EXO[OH]'}
 
 aerosol_name_dict = {
     'Water':{
