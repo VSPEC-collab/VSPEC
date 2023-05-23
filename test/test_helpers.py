fdd4140404db9786a89cdbd44ebbbfd9769cc0d3
Ted Johnson <tedjohnson12@g.ucla.edu>
2023-05-23 14:00:56 -0400

    add helpers for toggling psg

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -138,26 +138,76 @@ def test_get_transit_radius():
     assert predicted_radius == pytest.approx(
         true_radius, rel=0.1), 'test failed: ' + message
 
+def port_is_running(port:int,timeout:float,target_state:bool):
+    """
+    It takes some time to know if a port has been switched off.
+    We tell this function what we expect, and it listens for it.
+    Say we turn PSG off, it asks "Is PSG running?" for 10 seconds
+    until it hears back `False`. If it never does, it tells us that
+    PSG is still running.
+
+    Parameters
+    ----------
+    port : int
+        The port to listen on.
+    timeout : float
+        The length of time to listen for in seconds.
+    target_state : bool
+        The expected state at the end of listening.
+    
+    Returns
+    -------
+    psg_state : bool
+        The state of PSG. `True` if running on port `port`, else `False`.
+    """
+    timeout_time = time() + timeout
+    while True:
+        if target_state==True:
+            if helpers.is_port_in_use(port):
+                return True
+            elif time() > timeout_time:
+                return False
+        else:
+            if not helpers.is_port_in_use(port):
+                return False
+            elif time() > timeout_time:
+                return True
+
+
 def test_is_port_in_use():
     """
     Test `VSPEC.is_port_in_use`
     """
     default_psg_port = 3000
     timeout_duration = 10 # timeout after 10s
-    system('docker start psg')
-    timeout = time() + timeout_duration
-    while True:
-        if helpers.is_port_in_use(default_psg_port):
-            break
-        elif time() > timeout:
-            raise RuntimeError('Test failed -- timeout')
+    previous_state = helpers.is_port_in_use(default_psg_port)
     system('docker stop psg')
-    timeout = time() + timeout_duration
-    while True:
-        if not helpers.is_port_in_use(default_psg_port):
-            break
-        elif time() > timeout:
-            raise RuntimeError('Test failed -- timeout')
+    system('docker start psg')
+    if not port_is_running(default_psg_port,timeout_duration,True):
+        raise RuntimeError('Test failed -- timeout')
+    system('docker stop psg')
+    if port_is_running(default_psg_port,timeout_duration,False):
+        raise RuntimeError('Test failed -- timeout')
+    helpers.set_psg_state(previous_state)
+
+def test_set_psg_state():
+    """
+    Test `VSPEC.helpers.set_psg_state`
+    """
+    psg_port = 3000
+    timeout = 20
+    previous_state = helpers.is_port_in_use(psg_port)
+    helpers.set_psg_state(True)
+    assert port_is_running(psg_port,timeout,True)
+    helpers.set_psg_state(True)
+    assert port_is_running(psg_port,timeout,True)
+    helpers.set_psg_state(False)
+    assert not port_is_running(psg_port,timeout,False)
+    helpers.set_psg_state(False)
+    assert not port_is_running(psg_port,timeout,False)
+    helpers.set_psg_state(True)
+    assert port_is_running(psg_port,timeout,True)
+    helpers.set_psg_state(previous_state)
 
 def test_arrange_teff():
     """
@@ -245,6 +295,7 @@ if __name__ in '__main__':
     test_isclose()
     test_get_transit_radius()
     test_is_port_in_use()
+    test_set_psg_state()
     test_arrange_teff()
     test_plan_to_df()
     test_CoordinateGrid()
