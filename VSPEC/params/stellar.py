752728e653e4b7626375510bd1b05c25c46c93d6
Ted Johnson <tedjohnson12@g.ucla.edu>
2023-05-23 19:47:26 -0400

    added docs

--- VSPEC/params/stellar.py
+++ VSPEC/params/stellar.py
@@ -6,7 +6,8 @@ Stellar Parameters
 
 from pathlib import Path
 from astropy import units as u
-from typing import Union
+import numpy as np
+from VSPEC.config import stellar_area_unit
 
 from VSPEC.helpers import MSH
 
@@ -14,6 +15,13 @@ class LimbDarkeningParameters:
     """
     Limb Darkening Parameters for the Quadratic
     Limb Darkening Law
+
+    Parameters
+    ----------
+    u1 : float
+        The limb darkening parameter u1
+    u2 : float
+        The limb darkening parameter u2
 
     Attributes
     ----------
@@ -28,6 +36,8 @@ class LimbDarkeningParameters:
     .. math::
 
         \frac{I(\mu)}{I(1)} = 1 - u_1 (1-mu) - u_2 (1-mu)^2
+    
+    We use values taken from 
     """
     def __init__(self,u1:float,u2:float):
         self.u1 = u1
@@ -35,21 +45,21 @@ class LimbDarkeningParameters:
     @classmethod
     def solar(cls):
         """
-        From 2012A&A...546A..14C
+        From :cite:t:`2013A&A...552A..16C`
         S4 bandpass
         """
         return cls(0.0473,0.0841)
     @classmethod
     def proxima(cls):
         """
-        From 2012A&A...546A..14C
+        From :cite:t:`2012A&A...546A..14C`
         S4 bandpass
         """
         return cls(0.0551,0.1075)
     @classmethod
     def trappist(cls):
         """
-        From 2012A&A...546A..14C
+        From :cite:t:`2012A&A...546A..14C`
         S4 bandpass
         """
         return cls(0.0778,0.1619)
@@ -63,6 +73,64 @@ class LimbDarkeningParameters:
 class SpotParameters:
     """
     Spot Parameters
+
+    Parameters
+    -----------
+    distribution : str
+        The distribution function to be used for the spot positions. 'iso' or 'solar'.
+    initial_coverage : float
+        The coverage for a 'hot start'.
+    equillibrium_coverage : float
+        The fractional coverage of the star's surface by spots. This is the value
+        at growth-decay equillibrium, and different from the 'hot start' value given
+        by `initial_coverage`.
+    warmup : astropy.units.Quantity
+        The duration of the warmup period, during which the spot coverage approaches
+        equillibrium.
+    area_mean : astropy.units.quantity
+        The mean area of a spot on the star's surface is MSH.
+    area_logsigma : float
+        The standard deviation of the spot areas. This is a lognormal
+        distribution, so the units of this value are dex.
+    teff_umbra : astropy.units.Quantity
+        The effective temperature of the spot umbrae.
+    teff_penumbra : astropy.units.Quantity
+        The effective temperature of the spot penumbrae.
+    growth_rate : astropy.units.Quantity
+        The rate at which new spots grow.
+    decay_rate : astropy.units.Quantity
+        The rate at which existing spots decay.
+    initial_area : astropy.units.Quantity
+        The initial area of newly created spots.
+
+    Attrributes
+    -----------
+    distribution : str
+        The distribution function to be used for the spot positions. 'iso' or 'solar'.
+    initial_coverage : float
+        The coverage for a 'hot start'.
+    equillibrium_coverage : float
+        The fractional coverage of the star's surface by spots. This is the value
+        at growth-decay equillibrium, and different from the 'hot start' value given
+        by `initial_coverage`.
+    warmup : astropy.units.Quantity
+        The duration of the warmup period, during which the spot coverage approaches
+        equillibrium.
+    area_logmean : float
+        The mean area of a spot on the star's surface is MSH.
+    area_logsigma : float
+        The standard deviation of the spot areas. This is a lognormal
+        distribution, so the units of this value are dex.
+    teff_umbra : astropy.units.Quantity
+        The effective temperature of the spot umbrae.
+    teff_penumbra : astropy.units.Quantity
+        The effective temperature of the spot penumbrae.
+    growth_rate : astropy.units.Quantity
+        The rate at which new spots grow.
+    decay_rate : astropy.units.Quantity
+        The rate at which existing spots decay.
+    initial_area : astropy.units.Quantity
+        The initial area of newly created spots.
     """
     def __init__(
         self,
@@ -70,7 +138,7 @@ class SpotParameters:
         initial_coverage:float,
         equillibrium_coverage:float,
         warmup:u.Quantity,
-        area_logmean:u.LogQuantity,
+        area_mean:u.Quantity,
         area_logsigma:float,
         teff_umbra:u.Quantity,
         teff_penumbra:u.Quantity,
@@ -82,7 +150,7 @@ class SpotParameters:
         self.initial_coverage = initial_coverage
         self.equillibrium_coverage = equillibrium_coverage
         self.warmup = warmup
-        self.area_logmean = area_logmean
+        self.area_logmean = np.log10(area_mean/stellar_area_unit)
         self.area_logsigma = area_logsigma
         self.teff_umbra = teff_umbra
         self.teff_penumbra = teff_penumbra
@@ -91,6 +159,9 @@ class SpotParameters:
         self.initial_area = initial_area
         self.validate()
     def validate(self):
+        """
+        Validate class instance
+        """
         if self.distribution not in ['solar','iso']:
             raise ValueError('`distribution` must either be `solar` or `iso`')
         if self.initial_coverage > 1 or self.initial_coverage < 0:
@@ -136,6 +207,42 @@ class SpotParameters:
 class FaculaParameters:
     """
     Facula Parameters
+
+    Parameters
+    ----------
+    distribution : str
+        The distribution used to generate the faculae on the star. Currently only 'iso' is supported.
+    equillibrium_coverage : float
+        The fraction of the star's surface covered by the faculae at growth-decay equilibrium.
+    warmup : astropy.units.Quantity [time]
+        The warmup time for the faculae on the star to reach equilibrium.
+    mean_radius : astropy.units.quantity.Quantity [distance]
+        The mean radius of the faculae.
+    hwhm_radius : astropy.units.quantity.Quantity [distance]
+        The half-width at half-maximum radius of the faculae. It is the difference between the peak of the radius
+        distribution and the half maximum in the positive direction.
+    mean_timescale : astropy.units.quantity.Quantity [time]
+        The mean faculae lifetime.
+    hwhm_timescale : astropy.units.quantity.Quantity [time]
+        The facula timescale distribution half-width-half-maximum in hours. It is the difference between the peak of
+        the timescale distribution and the half maximum in the positive direction.
+
+    Attributes
+    ----------
+    distribution : str
+        The distribution used to generate the faculae on the star.
+    equillibrium_coverage : float
+        The fraction of the star's surface covered by the faculae at growth-decay equilibrium.
+    warmup : astropy.units.Quantity
+        The warmup time for the faculae on the star to reach equilibrium.
+    mean_radius : astropy.units.quantity.Quantity
+        The mean radius of the faculae.
+    hwhm_radius : astropy.units.quantity.Quantity
+        The half-width at half-maximum radius of the faculae.
+    mean_timescale : astropy.units.quantity.Quantity
+        The mean faculae lifetime.
+    hwhm_timescale : astropy.units.quantity.Quantity
+        The facula timescale distribution half-width-half-maximum in hours.
     """
     def __init__(
         self,
@@ -156,6 +263,9 @@ class FaculaParameters:
         self.hwhm_timescale = hwhm_timescale
         self.validate()
     def validate(self):
+        """
+        Validate class instance
+        """
         if self.distribution not in ['iso']:
             raise ValueError('`distribution` must be `iso`')
         if self.equillibrium_coverage > 1 or self.equillibrium_coverage < 0:
@@ -176,6 +286,45 @@ class FaculaParameters:
         )
 
 class FlareParameters:
+    """
+    Parameters
+    ----------
+    group_probability : float
+        The probability that a given flare will be closely followed by another flare.
+    teff_mean : astropy.units.quantity.Quantity [temperature]
+        The mean temperature of the flare blackbody.
+    teff_sigma : astropy.units.quantity.Quantity [temperature]
+        The standard deviation of the generated flare temperature.
+    fwhm_mean : astropy.units.LogQuantity
+        The mean logarithm of the full width at half maximum (FWHM) of the flare in days.
+    fwhm_sigma : float
+        The standard deviation of the logarithm of the FWHM of the flare in days.
+    E_min : astropy.units.LogQuantity
+        Log of the minimum energy flares to be considered in ergs.
+    E_max : astropy.units.LogQuantity
+        Log of the maximum energy flares to be considered in ergs.
+    E_steps : int
+        The number of flare energy steps to consider.
+
+    Attributes
+    ----------
+    group_probability : float
+        The probability that a given flare will be closely followed by another flare.
+    teff_mean : astropy.units.quantity.Quantity [temperature]
+        The mean temperature of the flare blackbody.
+    teff_sigma : astropy.units.quantity.Quantity [temperature]
+        The standard deviation of the generated flare temperature.
+    fwhm_mean : astropy.units.LogQuantity
+        The mean logarithm of the full width at half maximum (FWHM) of the flare in days.
+    fwhm_sigma : float
+        The standard deviation of the logarithm of the FWHM of the flare in days.
+    E_min : astropy.units.LogQuantity
+        Log of the minimum energy flares to be considered in ergs.
+    E_max : astropy.units.LogQuantity
+        Log of the maximum energy flares to be considered in ergs.
+    E_steps : int
+        The number of flare energy steps to consider.
+    """
     def __init__(
         self,
         group_probability:float,
@@ -215,6 +364,28 @@ class FlareParameters:
 class GranulationParameters:
     """
     Granulation Parameters
+
+    Parameters
+    ----------
+    mean : float
+        The mean coverage of low-teff granulation.
+    amp : float
+        The amplitude of granulation oscillations.
+    period : astropy.units.Quantity
+        The period of granulation oscillations.
+    dteff : astropy.units.Quantity
+        The difference between the quiet photosphere and the low-teff granulation region.
+
+    Attributes
+    ----------
+    mean : float
+        The mean coverage of low-teff granulation.
+    amp : float
+        The amplitude of granulation oscillations.
+    period : astropy.units.Quantity
+        The period of granulation oscillations.
+    dteff : astropy.units.Quantity
+        The difference between the quiet photosphere and the low-teff granulation region.
     """
     def __init__(
         self,
@@ -247,7 +418,62 @@ class GranulationParameters:
 class StarParameters:
     """
     Class to store stellar model parameters.
+
+    Parameters
+    ----------
+    template : str
+        The template used for the stellar model.
+    teff : astropy.units.quantity.Quantity
+        The effective temperature of the star.
+    mass : astropy.units.quantity.Quantity
+        The mass of the star.
+    radius : astropy.units.quantity.Quantity
+        The radius of the star.
+    period : astropy.units.quantity.Quantity
+        The rotational period of the star.
+    offset_magnitude : astropy.units.quantity.Quantity
+        The magnitude of the stellar rotation axis offset.
+    offset_direction : astropy.units.quantity.Quantity
+        The direction offset of the stellar rotation axis offset.
+    ld : LimbDarkeningParameters
+        The limb darkening parameters of the star.
+    spots : SpotParameters
+        The parameters of the spots on the star.
+    faculae : FaculaParameters
+        The parameters of the faculae on the star.
+    flares : FlareParameters
+        The parameters of the flares on the star.
+    granulation : GranulationParameters
+        The parameters of the granulation on the star.
+
+    Attributes
+    ----------
+    template : str
+        The template used for the stellar model.
+    teff : astropy.units.quantity.Quantity
+        The effective temperature of the star.
+    mass : astropy.units.quantity.Quantity
+        The mass of the star.
+    radius : astropy.units.quantity.Quantity
+        The radius of the star.
+    period : astropy.units.quantity.Quantity
+        The rotational period of the star.
+    offset_magnitude : astropy.units.quantity.Quantity
+        The magnitude of the stellar rotation axis offset.
+    offset_direction : astropy.units.quantity.Quantity
+        The direction offset of the stellar rotation axis offset.
+    ld : LimbDarkeningParameters
+        The limb darkening parameters of the star.
+    spots : SpotParameters
+        The parameters of the spots on the star.
+    faculae : FaculaParameters
+        The parameters of the faculae on the star.
+    flares : FlareParameters
+        The parameters of the flares on the star.
+    granulation : GranulationParameters
+        The parameters of the granulation on the star.
     """
+
     def __init__(
         self,
         template:str,
