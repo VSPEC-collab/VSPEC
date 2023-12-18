99aa87f6a90ad9026fa85c7d6206ea46eaad8793
Ted Johnson <ted.johnson@unlv.edu>
2023-12-18 14:38:19 -0800

    clean up psg api

--- test/pytest/test_psg_api.py
+++ test/pytest/test_psg_api.py
@@ -6,9 +6,9 @@ Tests for `VSPEC.psg_api` module
 from pathlib import Path
 import pytest
 from astropy import units as u
-import matplotlib.pyplot as plt
+from pypsg import PyRad
 
-from VSPEC.psg_api import call_api,call_api_from_file, PSGrad, get_reflected, parse_full_output
+from VSPEC.psg_api import call_api,call_api_from_file, get_reflected, parse_full_output
 from VSPEC.helpers import is_port_in_use,set_psg_state
 
 API_KEY_PATH = Path.home() / 'psg_key.txt'
@@ -17,25 +17,6 @@ VSPEC_CONFIG_PATH = Path(__file__).parent / 'default.yaml'
 PSG_PORT = 3000
 
 RAD_PATH = Path(__file__).parent / 'data' / 'transit_reflected'
-
-
-
-
-@pytest.mark.skipif(not API_KEY_PATH.exists(),reason='This test requires an API key')
-def test_call_api_nonlocal():
-    """
-    Run tests for `VSPEC.psg_api.call_api()`
-    This test expects that you have a file at
-    `~/` called `psg_key.txt` that contains your own
-    API key. Otherwise it is skipped.
-    """
-    psg_url = 'https://psg.gsfc.nasa.gov'
-    with open(API_KEY_PATH,'r',encoding='UTF-8') as file:
-        api_key = file.read()
-    data = '<OBJECT>Exoplanet\n<OBJECT-NAME>ProxCenb'
-    content = call_api(psg_url=psg_url,api_key=api_key,output_type='cfg',config_data=data)
-    assert b'<OBJECT>Exoplanet' in content
-    
     
 @pytest.mark.skipif(not is_port_in_use(3000),reason='PSG must be running locally to run this test')
 def test_call_api_local():
@@ -64,17 +45,13 @@ def test_call_api_from_file():
     set_psg_state(previous_state)
 
 
-def test_PSGrad():
+def test_pyrad():
     """
     Run tests for `VSPEC.psg_api.PSGrad()`
     """
     file = Path(__file__).parent / 'data' / 'test_rad.rad'
-    rad = PSGrad.from_rad(file)
-    assert isinstance(rad.header,dict)
-    assert isinstance(rad.data,dict)
-    with pytest.raises(ValueError):
-        file = Path(__file__).parent / 'data' / 'gcm_error.rad'
-        PSGrad.from_rad(file)
+    rad = PyRad.from_bytes(file.read_bytes())
+    assert isinstance(rad,PyRad)
 
 def test_get_reflected():
     """
@@ -83,27 +60,27 @@ def test_get_reflected():
     data_dir = Path(__file__).parent / 'data' / 'test_reflected'
     planet_name = 'proxima-Cen-b'
 
-    atm_cmb = PSGrad.from_rad(data_dir / 'atm_cmb.rad')
-    atm_therm = PSGrad.from_rad(data_dir / 'atm_therm.rad')
+    atm_cmb = PyRad.from_bytes((data_dir / 'atm_cmb.rad').read_bytes())
+    atm_therm = PyRad.from_bytes((data_dir / 'atm_therm.rad').read_bytes())
     ref = get_reflected(atm_cmb,atm_therm,planet_name)
     assert isinstance(ref,u.Quantity)
-    assert len(ref) == len(atm_cmb.data['Wave/freq'])
+    assert len(ref) == len(atm_cmb.wl)
 
-    no_atm_cmb = PSGrad.from_rad(data_dir / 'no_atm_cmb.rad')
-    no_atm_therm = PSGrad.from_rad(data_dir / 'no_atm_therm.rad')
+    no_atm_cmb = PyRad.from_bytes((data_dir / 'no_atm_cmb.rad').read_bytes())
+    no_atm_therm = PyRad.from_bytes((data_dir / 'no_atm_therm.rad').read_bytes())
     ref = get_reflected(no_atm_cmb,no_atm_therm,planet_name)
     assert isinstance(ref,u.Quantity)
-    assert len(ref) == len(atm_cmb.data['Wave/freq'])
+    assert len(ref) == len(atm_cmb.wl)
 
     # also works if just one has separated thermal+reflected columns
     ref = get_reflected(atm_cmb,no_atm_therm,planet_name)
     assert isinstance(ref,u.Quantity)
-    assert len(ref) == len(atm_cmb.data['Wave/freq'])
+    assert len(ref) == len(atm_cmb.wl)
     ref = get_reflected(no_atm_cmb,atm_therm,planet_name)
     assert isinstance(ref,u.Quantity)
-    assert len(ref) == len(atm_cmb.data['Wave/freq'])
+    assert len(ref) == len(atm_cmb.wl)
 
-    hires = PSGrad.from_rad(data_dir / 'hires_cmb.rad')
+    hires = PyRad.from_bytes((data_dir / 'hires_cmb.rad').read_bytes())
     with pytest.raises(ValueError):
         get_reflected(hires,atm_therm,planet_name)
 
