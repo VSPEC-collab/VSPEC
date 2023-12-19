332fb410c01995ee4962b5f67be6856d15554562
Ted Johnson <ted.johnson@unlv.edu>
2023-12-18 20:47:46 -0800

    change test to reflect new planet obs model

--- test/pytest/test_main.py
+++ test/pytest/test_main.py
@@ -59,7 +59,7 @@ def test_get_observation_plan(observation_model:ObservationModel):
     # create observation plan for planet
     observation_plan = observation_model.get_observation_plan(observation_parameters,planet=True)
     expected_N_obs = int((observation_model.params.obs.observation_time / observation_model.params.obs.integration_time \
-        / observation_model.params.psg.phase_binning))
+        / observation_model.params.psg.phase_binning)) + 1
     assert len(observation_plan['time']) == expected_N_obs
 
     # create observation plan for star
