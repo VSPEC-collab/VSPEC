64f6e44bee854afc7bce601ab202a2e851c0818b
Ted Johnson <ted.johnson@unlv.edu>
2023-12-22 19:30:05 -0800

    unskip waccm tests

--- test/pytest/test_waccm.py
+++ test/pytest/test_waccm.py
@@ -1,11 +1,12 @@
+
+from os import chdir
 from pathlib import Path
 import pytest
-from astropy import units as u
 import numpy as np
 import matplotlib.pyplot as plt
 
 import netCDF4 as nc
-from os import chdir
+
     
 from VSPEC.waccm.read_nc import validate_variables, get_time_index, time_unit,get_shape
 import VSPEC.waccm.read_nc as rw
@@ -13,50 +14,43 @@ import VSPEC.waccm.write_psg as wp
 
 chdir(Path(__file__).parent)
 
-DATA_PATH = Path('/Users/tjohns39/Documents/GCMs/WACCM/TR1e_flare_1yr_psg.nc')
+# DATA_PATH = Path('/Users/tjohns39/Documents/GCMs/WACCM/TR1e_flare_1yr_psg.nc')
+DATA_PATH = Path.home() / 'gcms' / 'waccm' / 'TR1e_flare_1month_var.nc'
 
 
 def read_TR1():
     path = Path('/Users/tjohns39/Documents/GCMs/WACCM/TR1e_flare_1yr_psg.nc')
     with nc.Dataset(path,'r',format='NETCDF4') as data:
         validate_variables(data)
-        0
-@pytest.mark.skip()
 def test_validate_vars():
     with nc.Dataset(DATA_PATH,'r',format='NETCDF4') as data:
         validate_variables(data)
 
-@pytest.mark.skip()
 def test_get_time_index():
     with nc.Dataset(DATA_PATH,'r',format='NETCDF4') as data:
         time = np.array(data.variables['time'][:])*time_unit
         for i,t in enumerate(time):
             assert get_time_index(data,t) == i
-@pytest.mark.skip()
 def test_get_shape():
     with nc.Dataset(DATA_PATH,'r',format='NETCDF4') as data:
         shape = get_shape(data)
         assert data.variables['T'].shape == shape
-@pytest.mark.skip()
 def test_surface_pressure():
     with nc.Dataset(DATA_PATH,'r',format='NETCDF4') as data:
         ps = rw.get_psurf(data,0)
         _,_,N_lat,N_lon = get_shape(data)
         assert ps.shape == (N_lat,N_lon)
-@pytest.mark.skip()
 def test_pressure():
     with nc.Dataset(DATA_PATH,'r',format='NETCDF4') as data:
         press = rw.get_pressure(data,0)
         _,N_layer,N_lat,N_lon = get_shape(data)
         assert press.shape == (N_layer,N_lat,N_lon)
-@pytest.mark.skip()
 def test_tsurf():
     with nc.Dataset(DATA_PATH,'r',format='NETCDF4') as data:
         tsurf = rw.get_tsurf(data,0)
         _,_,N_lat,N_lon = get_shape(data)
         assert tsurf.shape == (N_lat,N_lon)
 
-@pytest.mark.skip()
 def test_temperature():
     with nc.Dataset(DATA_PATH,'r',format='NETCDF4') as data:
         temp = rw.get_temperature(data,0)
@@ -65,7 +59,6 @@ def test_temperature():
         assert temp.shape == (N_layer,N_lat,N_lon)
         res = temp[0,:,:] - tsurf
         assert np.median(np.abs(res/tsurf)) < 0.1
-@pytest.mark.skip()
 def test_get_winds():
     with nc.Dataset(DATA_PATH,'r',format='NETCDF4') as data:
         U,V = rw.get_winds(data,0)
@@ -73,7 +66,6 @@ def test_get_winds():
         assert U.shape == (N_layer,N_lat,N_lon)
         assert V.shape == (N_layer,N_lat,N_lon)
 
-@pytest.mark.skip()
 def test_get_coords():
     with nc.Dataset(DATA_PATH,'r',format='NETCDF4') as data:
         lat,lon = rw.get_coords(data)
@@ -83,13 +75,11 @@ def test_get_coords():
         tsurf = rw.get_tsurf(data,0)
         plt.pcolormesh(lon,lat,tsurf)
 
-@pytest.mark.skip()
 def test_albedo():
     with nc.Dataset(DATA_PATH,'r',format='NETCDF4') as data:
         albedo = rw.get_albedo(data,0)
         _,_,N_lat,N_lon = get_shape(data)
         assert albedo.shape == (N_lat,N_lon)
-@pytest.mark.skip()
 def test_aerosol():
     with nc.Dataset(DATA_PATH,'r',format='NETCDF4') as data:
         water,water_size = rw.get_aerosol(data,0,'CLDLIQ','REL')
@@ -102,7 +92,6 @@ def test_aerosol():
         ice,ice_size = rw.get_ice(data,0)
         assert ice.shape == (N_layer,N_lat,N_lon)
         assert ice_size.shape == (N_layer,N_lat,N_lon)
-@pytest.mark.skip()
 def test_molecules():
     with nc.Dataset(DATA_PATH,'r',format='NETCDF4') as data:
         co2 = rw.get_molecule(data,0,'CO2')
@@ -114,44 +103,24 @@ def test_molecules():
         assert molecs['N2'].shape == (N_layer,N_lat,N_lon)
         assert np.all(np.abs(molecs['CO2']+molecs['H2O']+molecs['N2']-1) < 1e-6)
 
-@pytest.mark.skip()
 def test_write_cfg_params():
     with nc.Dataset(DATA_PATH,'r',format='NETCDF4') as data:
         molecs = rw.get_molecule_suite(data,0,['CO2','H2O'],background='N2')
         aerosols = ['Water','WaterIce']
         params = wp.get_cfg_params(data,molecs,aerosols)
         assert isinstance(params,dict)
-@pytest.mark.skip()
 def test_write_binary_array():
     with nc.Dataset(DATA_PATH,'r',format='NETCDF4') as data:
         b,mol = wp.get_binary_array(data,0,['CO2','H2O'],['Water','WaterIce'],'N2')
-        0
-@pytest.mark.skip()
 def test_get_cfg_contents():
     with nc.Dataset(DATA_PATH,'r',format='NETCDF4') as data:
         contents = wp.get_cfg_contents(data,0,['CO2','H2O'],['Water','WaterIce'],'N2')
         # call_api(output_type='set',app='globes',config_data=contents)
-    with open('../test_gcms/waccmt0.gcm','wb') as file:
+    wpath = Path(__file__).parent / 'temp.cfg'
+    with open(wpath,'wb') as file:
         file.write(contents)
-        0
+    wpath.unlink()
 
 
 if __name__ in '__main__':
-
-    read_TR1()
-    # test_validate_vars()
-    # test_get_time_index()
-    # test_get_shape()
-    # test_surface_pressure()
-    # test_pressure()
-    # test_tsurf()
-    # test_temperature()
-    # test_get_winds()
-    # test_get_coords()
-    # test_albedo()
-    # test_aerosol()
-    # test_molecules()
-    # test_write_cfg_params()
-    # test_write_binary_array()
-    test_get_cfg_contents()
-
+    pytest.main(args=[__file__])
