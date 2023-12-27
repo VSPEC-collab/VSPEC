cf693934efcffed7526558836c0a4ee55ee0c0ba
Ted Johnson <ted.johnson@unlv.edu>
2023-12-27 00:32:05 -0800

    Remove geometry helpers. They were moved to `vspec-vsm`

--- VSPEC/helpers/__init__.py
+++ VSPEC/helpers/__init__.py
@@ -7,5 +7,4 @@ throughout the rest of the package.
 from VSPEC.helpers.astropy_units import isclose
 from VSPEC.helpers.misc import get_transit_radius, get_planet_indicies
 from VSPEC.helpers.teff import arrange_teff, get_surrounding_teffs, round_teff, clip_teff
-from VSPEC.helpers.geometry import get_angle_between, proj_ortho, calc_circ_fraction_inside_unit_circle
 from VSPEC.helpers.files import check_and_build_dir, get_filename
