c7b9edf7f1b2668d414521a0af50d28275e60d3d
Ted Johnson <ted.johnson@unlv.edu>
2023-12-27 00:34:50 -0800

    remove tests for get_transit_radius The func was removed in previous commit

--- VSPEC/helpers/__init__.py
+++ VSPEC/helpers/__init__.py
@@ -5,6 +5,6 @@ This module contains functions that may be shared
 throughout the rest of the package.
 """
 from VSPEC.helpers.astropy_units import isclose
-from VSPEC.helpers.misc import get_transit_radius, get_planet_indicies
+from VSPEC.helpers.misc import get_planet_indicies
 from VSPEC.helpers.teff import arrange_teff, get_surrounding_teffs, round_teff, clip_teff
 from VSPEC.helpers.files import check_and_build_dir, get_filename
