024386abe8df7a46fdb5aedcbceaa36c4a068aa0
Ted Johnson <ted.johnson@unlv.edu>
2025-08-03 19:58:12 -0700

    Allow more general 3 layer atmosphere

--- test/pytest/test_gcm/test_gcm_twoface.py
+++ test/pytest/test_gcm/test_gcm_twoface.py
@@ -39,42 +39,46 @@ def test_clean_interp():
 
 
 def test_gen_planet():
-    dayside_pressure = 1 * u.bar
-    nightside_pressure = 2 * u.bar
-    elbow_pressure = 0.01 * u.bar
-    dayside_temperature = 800 * u.K
-    nightside_temperature = 500 * u.K
-    elbow_temperature = 400 * u.K
+    dayside_surface_pressure = 1 * u.bar
+    nightside_surface_pressure = 2 * u.bar
+    tropopause_pressure = 0.1 * u.bar
+    stratosphere_pressure = 0.01 * u.bar
+    dayside_surface_temperature = 800 * u.K
+    nightside_surface_temperature = 500 * u.K
+    tropopause_temperature = 400 * u.K
+    stratosphere_temperature = 100 * u.K
     top_pressure = 1e-5 * u.bar
-    n_linear = 30
-    n_const = 20
+    n_troposphere = 30
+    n_stratosphere = 10
+    n_isothermal = 20
     nphi = 90
     ntheta = 45
     scheme = 'cos2'
     dayside_h2o = 1e-2 * u.dimensionless_unscaled
     nightside_h2o = 1e-1 * u.dimensionless_unscaled
-    elbow_h2o = 1e-3 * u.dimensionless_unscaled
+    tropopause_h2o = 1e-3 * u.dimensionless_unscaled
+    stratosphere_h2o = 1e-3 * u.dimensionless_unscaled
     co2 = 1e-3 * u.dimensionless_unscaled
     o3 = 1e-3 * u.dimensionless_unscaled
     no2 = 1e-3 * u.dimensionless_unscaled
     albedo = 0.1
     planet: psg.globes.PyGCM = twoface.gen_planet(
-        dayside_pressure, elbow_pressure,
-        dayside_temperature, elbow_temperature,
-        nightside_pressure, elbow_pressure,
-        nightside_temperature, elbow_temperature,
-        n_linear, n_const, top_pressure, nphi, ntheta,
+        dayside_surface_pressure, tropopause_pressure, stratosphere_pressure,
+        dayside_surface_temperature, tropopause_temperature, stratosphere_temperature,
+        nightside_surface_pressure, tropopause_pressure, stratosphere_pressure,
+        nightside_surface_temperature, tropopause_temperature, stratosphere_temperature,
+        n_troposphere, n_stratosphere, n_isothermal, top_pressure, nphi, ntheta,
         scheme,
-        dayside_h2o, elbow_h2o,
-        nightside_h2o, elbow_h2o,
+        dayside_h2o, tropopause_h2o, stratosphere_h2o,
+        nightside_h2o, tropopause_h2o, stratosphere_h2o,
         co2, o3, no2, albedo
     )
 
-    assert planet.pressure.dat.shape == (n_linear+n_const, nphi, ntheta), \
-        f'Expected pressure shape to be {(n_linear+n_const, nphi, ntheta)}, \
+    assert planet.pressure.dat.shape == (n_troposphere+n_stratosphere+n_isothermal, nphi, ntheta), \
+        f'Expected pressure shape to be {(n_troposphere+n_stratosphere+n_isothermal, nphi, ntheta)}, \
         got {planet.pressure.dat.shape}'
-    assert planet.temperature.dat.shape == (n_linear+n_const, nphi, ntheta), \
-        f'Expected temperature shape to be {(n_linear+n_const, nphi, ntheta)}, \
+    assert planet.temperature.dat.shape == (n_troposphere+n_stratosphere+n_isothermal, nphi, ntheta), \
+        f'Expected temperature shape to be {(n_troposphere+n_stratosphere+n_isothermal, nphi, ntheta)}, \
         got {planet.temperature.dat.shape}'
     assert np.all(np.isclose(planet.pressure.dat[-1, :, :], top_pressure, rtol=1e-15)), \
         f'Expected top pressure to be {top_pressure}, \
@@ -82,19 +86,23 @@ def test_gen_planet():
             and {np.max(planet.pressure.dat[-1,:,:])}'
     assert np.isclose(np.min(planet.pressure.dat), top_pressure, rtol=1e-15), \
         f'Expected minimum pressure to be {top_pressure}, got {np.min(planet.pressure.dat)}'
-    assert np.max(planet.pressure.dat) == max(dayside_pressure, nightside_pressure), \
-        f'Expected maximum pressure to be {max(dayside_pressure, nightside_pressure)}, \
+    assert np.max(planet.pressure.dat) == max(dayside_surface_pressure, nightside_surface_pressure), \
+        f'Expected maximum pressure to be {max(dayside_surface_pressure, nightside_surface_pressure)}, \
             got {np.max(planet.pressure.dat)}'
-    assert np.all(np.isclose(planet.temperature.dat[n_linear:, :, :], elbow_temperature, rtol=1e-15)), \
-        f'Expected elbow temperature to be {elbow_temperature}, \
-            got values between {np.min(planet.temperature.dat[n_linear:, :, :])} \
-            and {np.max(planet.temperature.dat[n_linear:, :, :])}'
+    assert np.all(np.isclose(planet.temperature.dat[n_troposphere, :, :], tropopause_temperature, rtol=1e-15)), \
+        f'Expected tropopause temperature to be {tropopause_temperature}, \
+            got values between {np.min(planet.temperature.dat[n_troposphere:, :, :])} \
+            and {np.max(planet.temperature.dat[n_troposphere:, :, :])}'
+    assert np.all(np.isclose(planet.temperature.dat[(n_troposphere+n_stratosphere):, :, :], stratosphere_temperature, rtol=1e-15)), \
+        f'Expected stratosphere temperature to be {stratosphere_temperature}, \
+            got values between {np.min(planet.temperature.dat[(n_troposphere+n_stratosphere):, :, :])} \
+            and {np.max(planet.temperature.dat[(n_troposphere+n_stratosphere):, :, :])}'
     assert np.all(
-        planet.temperature.dat[0, :, :] >= min(dayside_temperature, nightside_temperature)), \
-        f'Expected surface pressures greater than {min(dayside_temperature, nightside_temperature)}, \
+        planet.temperature.dat[0, :, :] >= min(dayside_surface_temperature, nightside_surface_temperature)), \
+        f'Expected surface pressures greater than {min(dayside_surface_temperature, nightside_surface_temperature)}, \
         got values between {np.min(planet.temperature.dat[0,:,:])} and {np.max(planet.temperature.dat[0,:,:])}'
-    assert np.all(planet.temperature.dat[0, :, :] <= max(dayside_temperature, nightside_temperature)), \
-        f'Expected surface pressures less than {max(dayside_temperature, nightside_temperature)}, \
+    assert np.all(planet.temperature.dat[0, :, :] <= max(dayside_surface_temperature, nightside_surface_temperature)), \
+        f'Expected surface pressures less than {max(dayside_surface_temperature, nightside_surface_temperature)}, \
             got values between {np.min(planet.temperature.dat[0,:,:])} and {np.max(planet.temperature.dat[0,:,:])}'
 
 
