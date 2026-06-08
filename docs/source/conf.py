87b66488e42aab3911bd1ee16700edce53e9dc84
Ted Johnson <ted.johnson@unlv.edu>
2026-06-07 18:54:32 -0700

    Prep v1.2.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2025, The VSPEC Collaboration'
 author = 'Ted Johnson and Cameron Kelahan'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.0'
+release = '1.2.0'
 
 
 # -- General configuration ---------------------------------------------------
